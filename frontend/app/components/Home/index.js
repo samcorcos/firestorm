4c8e56a048c804200e0878eb66b3f5d5e0d0e468
Sam Corcos <sam@corcos.io>
2016-01-28 13:50:23 -0500

    fixing styling

--- frontend/app/components/Home/index.js
+++ frontend/app/components/Home/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import styles from './styles'
 
 const Home = React.createClass({
   render() {
-    return <div className={ styles.test }> Home </div>
+    return <div> Home </div>
   }
 })
 
